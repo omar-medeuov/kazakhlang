3b2dc286404d2a8374325122b8a9a3a84fb4110c
omar-medeuov <omarmedeuov@gmail.com>
2025-06-14 03:26:11 +0500

    functioning blog app, no style

---
+++ blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+
+from wagtail.search import index
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
+    content_panels = Page.content_panels + ["intro"]
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + ["date", "intro", "body"]
