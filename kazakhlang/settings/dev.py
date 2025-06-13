74281a6b48350fd2ecdd9c2920b8f2b79bf94792
omar-medeuov <omarmedeuov@gmail.com>
2025-06-14 01:10:45 +0500

    initial commit

---
+++ kazakhlang/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-fmwnu&v0ow$fs%rh%#4c-@wj!o8q$1obvi69osn*q(00*ny=&#"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
