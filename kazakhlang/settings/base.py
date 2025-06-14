2254b62974ed86b0211fc8647e6b161cc265d40e
omar-medeuov <omarmedeuov@gmail.com>
2025-06-14 21:12:49 +0500

    settings SECRET_KEY, .env

--- kazakhlang/settings/base.py
+++ kazakhlang/settings/base.py
@@ -13,8 +13,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
+from dotenv import load_dotenv
+
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+load_dotenv(os.path.join(PROJECT_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
